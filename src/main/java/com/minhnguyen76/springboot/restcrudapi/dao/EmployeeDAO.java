ed6d3022e498c8ad4de600d97e7b842c0186e1f1
minhckd18 <127463458+minhckd18@users.noreply.github.com>
2023-06-12 15:39:19 +0700

    sorry, I didn't add all the changes

--- src/main/java/com/minhnguyen76/springboot/restcrudapi/dao/EmployeeDAO.java
+++ src/main/java/com/minhnguyen76/springboot/restcrudapi/dao/EmployeeDAO.java
@@ -1,2 +1,11 @@
-package com.minhnguyen76.springboot.restcrudapi.dao;public interface EmployeeDAO {
+package com.minhnguyen76.springboot.restcrudapi.dao;
+
+import com.minhnguyen76.springboot.restcrudapi.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    // Read all employees
+    List<Employee> findAll();
 }
--- src/main/java/com/minhnguyen76/springboot/restcrudapi/rest/EmployeeRestController.java
+++ src/main/java/com/minhnguyen76/springboot/restcrudapi/rest/EmployeeRestController.java
@@ -1,2 +1,28 @@
-package com.minhnguyen76.springboot.restcrudapi.rest;public class EmployeeRestController {
+package com.minhnguyen76.springboot.restcrudapi.rest;
+
+import com.minhnguyen76.springboot.restcrudapi.dao.EmployeeDAO;
+import com.minhnguyen76.springboot.restcrudapi.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    // inject an EmployeeDAO
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+       return  employeeDAO.findAll();
+    }
 }
