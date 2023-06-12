c1a953a0a53dfcc056624a345b80cdaf4d720a2d
unknown <nguyen.hoangminh764@gmail.com>
2023-06-12 08:37:56 +0700

    First commit, init project with spring initializr, use sql script to init database

---
+++ src/test/java/com/minhnguyen76/springboot/restcrudapi/RestCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhnguyen76.springboot.restcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
